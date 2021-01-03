d6ceb6f2ed467ae9e9f1c6f32b6e2b182b69c380
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-03 20:33:48 -0300

    Delete consoleLog.js

--- consoleLog.js
+++
@@ -1,3 +0,0 @@
-console.log('Hello world!', 2021, 'Início de uma nova década');
-console.log('Meu nome é "Paulo Queiroz". Estou treinando Javascript às '+ 20 +':00','h');
-//Primeiras aulas do curso de javascript e typescript do professor Luis Otávio Miranda na Udemy
