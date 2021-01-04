591ccf70011467d39935ddc46b35262684f89515
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-03 22:59:19 -0300

    aula sobre variavel do tipo let

---
+++ javascript-basico/variaveisComLet.js
@@ -0,0 +1,9 @@
+//variáveis precisam de nomes significativos
+//Variveis no javascript não podem ter traços
+let name = "João"
+
+console.log(name, 'nasceu em 1984');
+console.log('Em 2000', name,'conheceu Maria');
+console.log(name,'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', name,'em 2015');
+console.log('O filho de',name, 'se chama Eduardo');
