d91084f070cfc68b8d593e57e7c3729f2d721edc
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-04 23:24:23 -0300

    aula sobre variavel do tipo const e introdução sobre a utilização do typeof

---
+++ javascript-basico/variaveisComConst.js
@@ -0,0 +1,17 @@
+const name = 'João';
+console.log(name);
+
+const firstNumber = 10;
+const secondNumber = 5;
+
+const addition =  firstNumber + secondNumber;
+
+console.log(addition);
+
+const duplicateResult = addition * 2;
+
+console.log(duplicateResult);
+
+// typeof descobrir o tipo de uma variável
+
+console.log(typeof(name), typeof(duplicateResult));
