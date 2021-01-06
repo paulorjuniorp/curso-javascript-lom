4ff1bbb3364ff64b4b51b0a6d091da1723970eac
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-05 23:26:31 -0300

    Aula sobre tipos primitivos: código com tipos primitivos do javascript

---
+++ javascript-basico/tiposPrimitivos.js
@@ -0,0 +1,9 @@
+const nome = "João"; // string
+const nome1 = 'João'; // string
+const nome2 = `João`; // string
+
+const num1 = 10; // number
+const num2 = 10.52; // number
+
+let nomeAluno; // undefined
+let sobrenomeAluno = null; // nulo
