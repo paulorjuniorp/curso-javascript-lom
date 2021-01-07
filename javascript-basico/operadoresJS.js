b900c1f7a2048cdb71cfe8bed5a6c3c2288031d8
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-06 23:36:23 -0300

    Aula sobre operadores no javascript: aritmético, atribuição e incremento

--- javascript-basico/operadoresJS.js
+++ javascript-basico/operadoresJS.js
@@ -2,8 +2,16 @@
 // + - * /
 // ** potenciação
 // % resto de uma divisão
+// Precedência de operadores no cálculo: (), **, * /, + -
 
 const num1 = 20;
 const num2 = 30;
 
 console.log(num1 + num2);
+
+let contador = 1;
+console.log(contador++); // faz a soma após a ação
+console.log(++contador);//primeiro faz a soma e depois outra ação
+
+console.log(contador--); // faz a soma após a ação
+console.log(--contador);
