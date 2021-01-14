f7e6a8c127fc55e6ee62e390300449eff7b9519b
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-13 22:50:46 -0300

    Aula sobre alert, confirm e prompt utilizando o navegador com javascript: exercício com prompt

--- javascript-basico/alertConfirmPromp.js
+++ javascript-basico/alertConfirmPromp.js
@@ -4,3 +4,13 @@ let confirma = confirm('Vai sair?');
 console.log(confirma);
 let nome = prompt('Então qual o seu nome?');
 console.log(nome);
+
+let num1 = prompt("Digite um número");
+let num2 = prompt("Digite um número");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+let resultado = num1 + num2;
+
+console.log("A soma de",num1,"+",num2,`é ${resultado}`);
