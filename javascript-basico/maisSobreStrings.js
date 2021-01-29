044b25cf123dd2bba77deab707ab5f327f82e722
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-28 21:16:29 -0300

    Mais uma aula sobre string

--- javascript-basico/maisSobreStrings.js
+++ javascript-basico/maisSobreStrings.js
@@ -4,5 +4,9 @@ console.log(umaString);
 let outraString = "Um texto";
 console.log(outraString[4]);
 console.log(outraString.charAt(6));
-
+//retorna o começo de uma string
 console.log(outraString.indexOf('texto'));
+// retorna o começo de uma string de trás pra frente
+console.log(outraString.lastIndexOf('x'));
+
+console.log(outraString.match(/[a-z]/g));
