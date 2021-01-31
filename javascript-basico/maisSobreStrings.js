04898ff1ffa72936cbf45b7e7256edf4880acf7d
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-31 14:34:16 -0300

    Mais uma aula sobre string

--- javascript-basico/maisSobreStrings.js
+++ javascript-basico/maisSobreStrings.js
@@ -2,6 +2,9 @@ let umaString = "Um \"texto\""; // \ invertida pode ser utilizado como caractere
 console.log(umaString);
 
 let outraString = "Um texto";
+
+let maisUmaString = "O rato roeu a roupa do rei de Roma";
+
 console.log(outraString[4]);
 console.log(outraString.charAt(6));
 //retorna o começo de uma string
@@ -18,3 +21,12 @@ console.log(outraString.search(/[x]/g));
 
 //Troca o primeiro parâmetro pelo segundo em uma string
 console.log(outraString.replace('Um', 'Outro'));
+
+//replaca utilizando expressões regulares
+console.log(maisUmaString.replace(/r/g,'l'));
+
+//retorna o tamanho da string
+console.log(maisUmaString.length);
+
+//retorna uma parte da string
+console.log(maisUmaString.slice(2,6));
