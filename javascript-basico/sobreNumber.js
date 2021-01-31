626ed32fc5a567cb5dacfeb78b9fe95f58cef23e
paulorjuniorp <paulorjuniorp@gmail.com>
2021-01-31 20:25:15 -0300

    Exercício com strings

---
+++ javascript-basico/sobreNumber.js
@@ -0,0 +1,16 @@
+let num1 = 1;
+let num2 = 2.5;
+let numBinario = 1500;
+let num3 = 10.92356288294;
+
+console.log(num1.toString() + num2);
+console.log(typeof num1);
+
+//Exibe o numBinario em formato binário
+console.log(numBinario.toString(2));
+
+//exibe o numero float com duas casa decimais
+console.log(num3.toFixed(2));
+
+//checar se um valor é inteiro
+console.log(Number.isInteger(num1));
