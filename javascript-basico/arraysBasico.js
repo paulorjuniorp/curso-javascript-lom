ab9d49785d6764d47170dbc581b70019c467226c
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-03 23:09:36 -0300

    Aula com conteúdo básico sobre array

---
+++ javascript-basico/arraysBasico.js
@@ -0,0 +1,9 @@
+const alunos = ['Luiz','Maria','João'];
+console.log(alunos);
+
+// Se não existe o valor o array cria o índice e adiciona o elemento
+alunos[3] = 'Luiza';
+console.log(alunos);
+
+//saber o tamanho do array
+console.log(alunos.length);
