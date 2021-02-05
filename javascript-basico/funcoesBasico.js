8777f2035ebeb7b5f0dd6d005ed5d051a3a9236b
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-05 12:53:04 -0300

    Aula com conteúdo básico sobre funções

---
+++ javascript-basico/funcoesBasico.js
@@ -0,0 +1,17 @@
+function saudacao(nome){
+    return `Hello, ${nome}!`;
+}
+
+saudacao("Juinu");
+
+const variavel = saudacao('Latifa');
+
+console.log(variavel);
+
+function soma(x,y){
+    const resultado = x + y;
+    return resultado;
+}
+
+const somaValor = soma(2,9);
+console.log(somaValor);
