cc574d49ce17b4be21fe97c9b8f80757b74fd6bf
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-08 23:47:11 -0300

    Aula com conteúdo básico sobre objetos

---
+++ javascript-basico/objetoBasico.js
@@ -0,0 +1,5 @@
+const array = [1, 2, 3];
+
+// Com o const os valores dentro dos colchetes podem ser alterados, porém tirar os colchetes
+// e atribuir um novo valor não pode. Ex: const array = [1,2,2] => array = 123 não pode ser feito;
+
