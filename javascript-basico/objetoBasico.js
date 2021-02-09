7ef43f944a75181f620b23c62923e782cb0534c0
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-09 00:05:20 -0300

    Aula com conteúdo básico sobre objetos

--- javascript-basico/objetoBasico.js
+++ javascript-basico/objetoBasico.js
@@ -10,3 +10,30 @@ const pessoa = {
     idade: "7"
 };
 
+// Para facilitar e automatizar a criação de objetos, é possível criar uma função que faça tudo uma única vez
+
+function criarPessoa(nome, sobrenome, idade){
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    };
+};
+
+const pessoa1 = criarPessoa("Luiza", "Santos", 20);
+console.log(pessoa1.nome);
+
+// Em um objeto é possível criar uma função e dentro da função utilizar os elementos do objeto a partir
+// do comando this
+
+const pessoa2 = {
+    nome: 'Jéssica',
+    sobrenome: 'Oliveira',
+    idade: 23,
+
+    profissao(){
+        console.log(`${this.nome} ${this.sobrenome} tem apenas ${this.idade} anos de idade e já é médica`);
+    }
+};
+
+pessoa2.profissao();
