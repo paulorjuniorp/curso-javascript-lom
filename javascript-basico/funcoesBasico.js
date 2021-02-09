45a45ccd1adf1a18fa848e489f06c4fc7483bfcb
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-08 23:20:50 -0300

    Aula com conteúdo básico sobre funções

--- javascript-basico/funcoesBasico.js
+++ javascript-basico/funcoesBasico.js
@@ -15,3 +15,15 @@ function soma(x,y){
 
 const somaValor = soma(2,9);
 console.log(somaValor);
+
+// é possível colocar valores padrão como parâmtro de uma função para os casos onde nenhum valor é passado pelo usuário
+
+function multiplicar(x = 2, y =2){
+    const resultado = x * y;
+    return resultado;
+}
+
+const valorMulti = multiplicar();
+console.log(valorMulti);
+const outraMulti = multiplicar(5,3);
+console.log(outraMulti);
