a58e6b33fe2a236fd8e13a6ac173168b7fab7ce1
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-09 21:18:50 -0300

    Aula com conteúdo sobre tipos primitivos e valores referências

--- javascript-basico/valorPrimitivoValorReferencia.js
+++ javascript-basico/valorPrimitivoValorReferencia.js
@@ -1,9 +1,23 @@
 /*
 Primitivos (imutáveis) - string, number, boolean, undefined, null
 
+Referência (mutável) - array, object, function
 */
 
 let nome = 'Mundo';
 nome[0] = 'F';
 
 console.log(nome[0], nome);
+// Os valores são copiados
+let a = 'A';
+let b = a;
+console.log(a, b);
+a = 'B';
+console.log(a, b);
+// Os valores são passados a partir de um mesmo endereço na memória
+// por isso são mutáveis
+let a1 = [1,2,3];
+let b1 = a1;
+console.log(a1, b1);
+a1.push(4);
+console.log(a1, b1);
