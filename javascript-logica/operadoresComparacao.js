fc2090c3e2041ca6d6aaca3cf18146fec7914ee6
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-10 22:01:01 -0300

    Módulo de lógica: operadores de comparação

--- javascript-logica/operadoresComparacao.js
+++ javascript-logica/operadoresComparacao.js
@@ -15,3 +15,15 @@ console.log(5 <= 10);
 
 const valor2 = 20 <= 4;
 console.log(valor2);
+
+// Operador igual ==
+
+const num1 = 10;
+const num2 = 10;
+const valor3 = num1 == num2;
+console.log(valor3);
+// O problema é que mesmo que os valores sejam iguais, mas o tipo seja diferente ele retorna true
+const num3 = 20;
+const num4 = '20';
+const valor4 = num3 == num4;
+console.log(valor4);
