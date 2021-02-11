2b05353c3370a497602442c7707d579fa3b95791
paulorjuniorp <paulorjuniorp@gmail.com>
2021-02-10 21:55:21 -0300

    Módulo de lógica: operadores de comparação

--- javascript-logica/operadoresComparacao.js
+++ javascript-logica/operadoresComparacao.js
@@ -1,6 +1,17 @@
 // Operador maior que
-
 console.log(10 > 5);
 
 const valor = 20 > 4;
 console.log(valor);
+
+// Operador Maior que ou igual a
+console.log(10 >= 5);
+
+const valor1 = 20 >= 4;
+console.log(valor1);
+
+// Operador Menor que ou igual a
+console.log(5 <= 10);
+
+const valor2 = 20 <= 4;
+console.log(valor2);
